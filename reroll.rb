b1328bae2580f42c6bb1099d437a9564bd08e714
Gaelan Steele <gbs@canishe.com>
2013-07-15 18:03:16 -0400

    Pick a comment.

--- reroll.rb
+++ reroll.rb
@@ -5,20 +5,30 @@ issue_number = ARGV[0]
 issue = open "https://drupal.org/node/#{issue_number}/project-issue/json" do |json|
     JSON.load json
 end
+attachments = issue["attachments"]
+comments = issue["comments"]
 # Add comment numbers to comments
-issue["comments"].each do |id, comment|
-    comment["comment_number"] = comment["subject"][/#([0-9]+)/, 1].to_i
+comments.each do |id, comment|
+    comment["commentNumber"] = comment["subject"][/#([0-9]+)/, 1].to_i
 end
 # Convert the attachments from a hash to a array.
-issue["attachments"] = issue["attachments"].values
+attachments = attachments.values
 # Drop non-patch attachments because we don't need them.
-issue["attachments"].select! do |attachment|
+attachments.select! do |attachment|
     attachment["urls"].select! do |url|
         !(url =~ /\.patch$/)
     end
     attachment["urls"].size != 0
 end
 # Sort attachments by comment order
-issue["attachments"].sort_by! do |attatchment|
-    attatchment["comment_number"]
+attachments.sort_by! do |attatchment|
+    attatchment["commentNumber"]
 end
+# Decide on an attachment.
+chosen_attachment = if ARGV[1]
+                        number = ARGV[1].to_i
+                        attachments.select { |attachment| comments[attachment["commentId"]]["commentNumber"] == number }.to_i
+                    else
+                        attachments.last
+                    end
+ap chosen_attachment
