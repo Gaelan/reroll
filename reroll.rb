7deb6b09f09f177f1782e5c2c964cf7518ef3eae
Gaelan Steele <gbs@canishe.com>
2013-07-15 23:54:40 -0400

    Er, stuff.
    But mostly commit and rebase.

--- reroll.rb
+++ reroll.rb
@@ -5,9 +5,14 @@ issue_number = ARGV[0]
 issue = open "https://drupal.org/node/#{issue_number}/project-issue/json" do |json|
     JSON.load json
 end
-def cmd command
+def cmd command, return_result = false
     puts command
-    `#{command}`
+    if return_result
+        result = `#{command}`
+    else
+        system command
+    end
+    result
 end
 attachments = issue["attachments"]
 comments = issue["comments"]
@@ -35,10 +40,25 @@ chosen_attachment = if ARGV[1]
                     else
                         attachments.last
                     end
+if chosen_attachment["urls"].count != 1
+    puts "This comment has more than one patch. Bug Gaelan to make this work."
+    exit 1
+end
 # Get the date.
 date = comments[chosen_attachment["commentId"]]["created"]
 # Get the commit it worked on.
 puts "If this breaks, make sure you are in a git clone of D8."
-commit_hash = cmd "git log --before=#{date} -1 --pretty=format:%H"
+commit_hash = cmd "git log --before=#{date} -1 --pretty=format:%H", true
 # Make a branch.
 cmd "git checkout -b #{issue_number} #{commit_hash}"
+# Apply the patch
+cmd "curl #{chosen_attachment["urls"].values[0]}|git apply --index"
+# Commit it
+cmd "git add *"
+cmd "git commit -m #{comments[chosen_attachment["commentId"]]["commentNumber"]}"
+# Rebase!
+cmd "git rebase 8.x"
+puts "I will now start your shell. Resolve any conflicts, then exit."
+system ENV["SHELL"]
+cmd "git rebase --continue"
+puts "We're done! Make a patch and put it up on d.o. Don't forget to set the status to needs review, and remove the Needs reroll tag!"
